bd4f4686d61d704fedb11af19485303b25ba43a4
Almir Rogério Camolesi <camolesi@fema.edu.br>
2026-02-28 13:54:26 -0300

    Atualizaçaõ Construtores e Encapsulamento
    Atualizaçaõ Construtores e Encapsulamento

--- Viagem.cpp
+++ Viagem.cpp
@@ -3,16 +3,33 @@
 using namespace std;
 #include "Viagem.h"
 
-int main(){
-   Viagem v1;  //instanciacao objeto v1
-   v1.imprimir(); //invocacao método imprimir()
+int main (){
+    Viagem v1;
+    v1.imprimir();
+    Viagem v2;
 
-   Viagem v2 (10, "Assis/SP", 3, 10568.3,11256.9);
-   v2.imprimir();
-
-   Viagem v3 (12, "Assis/Bauru", 2, 45327.6 );
+   Viagem v3(10, "Assis/SP", 3, 54278.8, 54760.4);
    v3.imprimir();
 
-   v3.setKmChegada(45510);
-   v3.imprimir();
+   Viagem v5 (12, 2, "Assis/Marilia", 54278.8, 54760.4);
+   v5.imprimir();
+
+   Viagem v4 (12, "Assis/Bauru", 1, 63127);
+   v4.imprimir();
+
+   //v4.kmChegada = -5000;
+   //não permite alterar, pois o atributo está encapsulado
+
+   v4.setKmChegada(-5000);
+   v4.imprimir();
+
+   cout << "\nDescricao da viagem v4: " << v4.getDescricao() << endl;
+   cout << "\nTipo de Combustível de v4: " << v4.getTipoComb() << " - "
+                                        << v4.getDescTipoComb() << endl;
+
+   cout << "\nDistancia v5: " << v5.getDistancia() << endl;
+
+   Viagem v6(-8,"Assis/CM", 9, -3454, -43435);
+   v6.imprimir();
+
 }
--- Viagem.h
+++ Viagem.h
@@ -1,62 +1,140 @@
 //Viagem.h
+
 class Viagem{
-  private: //atributos
-    int id;
-    string descricao;
-    int tpComb; // 1-Gas, 2-Diesel, 3-Etanol
-    float kmSaida;
-    float kmChegada;
-  public: //métodos
-    Viagem(); // prototipo do construtor
-    //prototipo de um construtor parametrizado para Viagem
-    Viagem(int, string, int, float, float);
-    Viagem(int, string, int, float);
+   private: //atributos
+     int id;
+     string descricao;
+     int tipoComb; //1-Gas, 2-Diesel, 3-Etanol
+     float kmSaida;
+     float kmChegada;
 
-    //métodos get
+   public: //
+     Viagem(); // protótipo do método Construtor padrão para Viagem
 
-    //métodos set
-    void setKmChegada(float chegada){
-       this->kmChegada = chegada;
-    }
+     //protótipo do construtor parametrizado para Viagem
+     Viagem(int, string, int, float, float);
+     Viagem(int, int, string,  float, float);
+     Viagem(int, string, int, float);
 
-    void imprimir();
+     //métodos para acesso aos valores dos atributos - geters
+     int getId() {return this->id;}
+     string getDescricao() {return this->descricao;}
+     int getTipoComb() {return this->tipoComb;}
+     float getKmSaida() {return this->kmSaida;}
+     float getKmChegada() {return this->kmChegada;}
+     string getDescTipoComb();
+     float getDistancia();
 
+
+     //métodos de alteração/modificação - seters
+     void setId(int);
+     void setDescricao(string);
+     void setTipoComb(int);
+     void setKmSaida(float);
+     void setKmChegada(float);  //assinatura ou protótipo método setKmChegada
+
+     void imprimir();
 };
 
+//implementação dos construtores
 
-Viagem::Viagem(){ //Viagem padrão
-    this->id = 0;
-    this->descricao = "Viagem padrao";
-    this->tpComb = 0;
-    this->kmSaida = 0;
-    this->kmChegada = 0;
+Viagem::Viagem () { //construtor padrão
+   //cout << "\nPassei no construtor padrao\n";
+   this->id = 0;
+   this->descricao = "Viagem Padrao";
+   this->tipoComb = 0;
+   this->kmSaida = 0;
+   this->kmChegada = 0;
 }
 
-// construtor parametrizado com 5 parametros
 Viagem::Viagem(int id, string desc, int tipo, float saida, float chegada){
+    setId(id); // this->id = id;
+    setDescricao(desc); //this->descricao = desc;
+    setTipoComb(tipo); // this->tipoComb = tipo;
+    setKmSaida(saida); //this->kmSaida = saida;
+    setKmChegada(chegada); //this->kmChegada = chegada;
+}
+
+Viagem::Viagem(int id,  int tipo, string desc, float saida, float chegada){
     this->id = id;
     this->descricao = desc;
-    this->tpComb = tipo;
+    this->tipoComb = tipo;
     this->kmSaida = saida;
     this->kmChegada = chegada;
 }
 
-// construtor parametrizado com 4 parametros
 Viagem::Viagem(int id, string desc, int tipo, float saida){
     this->id = id;
     this->descricao = desc;
-    this->tpComb = tipo;
+    this->tipoComb = tipo;
     this->kmSaida = saida;
     this->kmChegada = 0;
 }
 
+//implementação métodos geters
+string Viagem::getDescTipoComb(){
+   string desc;
+   if (this->tipoComb == 1)
+        desc = "Gasolina";
+  else if (this->tipoComb == 2)
+          desc = "Diesel";
+       else if (this->tipoComb == 3)
+                desc = "Etanol";
+            else desc = "tipo combustivel não informado";
+
+     return desc;
+}
+
+float Viagem::getDistancia(){
+   float distancia = (this->kmChegada - this->kmSaida);
+   return distancia;
+}
+
+//implementação dos métodos seters
+void Viagem::setId(int id){
+    if (id > 0)
+        this->id = id;
+    else { this->id = 0;
+           cout << "\nValor de id invalido...\n";
+         }
+}
+
+void Viagem::setDescricao (string descricao){
+    this->descricao = descricao;
+}
+
+void Viagem::setTipoComb(int tipo){
+    if (tipo > 0 && tipo < 4)
+        this->tipoComb = tipo;
+    else this->tipoComb = 0;
+}
+
+void Viagem::setKmSaida(float saida){
+   if (saida>0)
+    this->kmSaida = saida;
+   else this->kmSaida = 0;
+}
+
+void Viagem::setKmChegada(float chegada){
+   if (chegada > this->kmSaida)
+       this->kmChegada = chegada;
+   else { this->kmChegada = 0;
+          cout << "\nKm Chegada tem que ser maior que a saida\n";
+        }
+}
+
+
 
 void Viagem::imprimir(){
-  cout << "\nImprimir viagem\n";
-  cout << "ID: " << this->id << endl;
-  cout << "Descricao: " << this->descricao << endl;
-  cout << "Tipo Combustivel: " << this->tpComb << endl;
-  cout << "Km de Saida: " << this->kmSaida << endl;
-  cout << "Km de Chegada: " << this-> kmChegada << endl;
+    cout << "\nImprimir Viagem\n";
+    cout << "ID: " << this->id << endl;
+    cout << "Descricao: " << this->descricao << endl;
+    cout << "Tipo Combustivel: " << this->tipoComb << " - "
+                                 << this->getDescTipoComb() << endl;
+    cout << "Km de Saida: " << this->kmSaida << endl;
+    cout << "Km de Chegada: " << this->kmChegada << endl;
 }
+
+
+
 
